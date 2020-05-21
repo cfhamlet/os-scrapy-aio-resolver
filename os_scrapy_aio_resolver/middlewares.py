74b97dbd85d6adeba08b069f794651952025d28b
Ozzy <cfhamlet@gmail.com>
2020-05-21 20:47:48 +0800

    trivial: pass

--- os_scrapy_aio_resolver/middlewares.py
+++ os_scrapy_aio_resolver/middlewares.py
@@ -53,7 +53,7 @@ class OsScrapyAioResolverSpiderMiddleware:
             yield r
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class OsScrapyAioResolverDownloaderMiddleware:
@@ -100,4 +100,4 @@ class OsScrapyAioResolverDownloaderMiddleware:
         pass
 
     def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+        spider.logger.info("Spider opened: %s" % spider.name)
